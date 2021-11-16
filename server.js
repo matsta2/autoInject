4171a6d90a7d25f1d026b4e0d993647263434622
Matas <matasstk@gmail.com>
2021-11-16 16:47:16 +0200

    auth0 integration

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 // create express app
 const app = express();
 // Setup server port
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 3000;
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
 // parse requests of content-type - application/json
 app.use(bodyParser.json())
 // define a root route
+const { auth, requiresAuth } = require('express-openid-connect');
+app.use(
+  auth({
+    authRequired: false,
+    auth0Logout : true,
+    issuerBaseURL: process.env.ISSUER_BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    baseURL: process.env.BASE_URL,
+    secret: process.env.SECRET,
+  })
+);
+
 app.get('/', (req, res) => {
-  res.send("Hello World");
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
 });
 // Requireroutes
 const detaleRoutes = require('./src/routes/detale.route')
